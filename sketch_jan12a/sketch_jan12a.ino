d3b5b7c3786de0681a87a6a5ce14628ea98b5d4a
chen jiyan <3036789610@qq.com>
2025-01-12 10:29:43 +0800

    modified:   sketch_jan12a/sketch_jan12a.ino

--- sketch_jan12a/sketch_jan12a.ino
+++ sketch_jan12a/sketch_jan12a.ino
@@ -1,6 +1,13 @@
 
 
-double move_f(int x);//手柄的输入转换曲线（也可以是直线）
+double move_f(int x)//手柄的输入转换曲线（也可以是直线）x是0~127的数字
+{
+  if(x<20) return x/2.0;
+  if(x>=20&&x<=80) return (x-20)*5.0/6.0+10;//(20,10) (80,60)
+  if(x>80) return (x-80)*40.0/47.0+60;//(80,60) (127,100)
+
+}
+
 void up(int x);//向前单位
 void down(int x);//向后单位
 void left(int x);//左转
@@ -12,7 +19,7 @@ void motor1(int x);//单独控制电机模块
 void motor2(int x);
 void motor3(int x);
 void motor4(int x);
-
+//舵机的180~输入的100
 void setup() {
   // put your setup code here, to run once:
    
