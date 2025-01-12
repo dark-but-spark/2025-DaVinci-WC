3cd4a87d42ee558686bb99c1e333201758c1d1b4
chen jiyan <3036789610@qq.com>
2025-01-12 10:15:54 +0800

    new file:   sketch_jan12a/sketch_jan12a.ino

---
+++ sketch_jan12a/sketch_jan12a.ino
@@ -0,0 +1,24 @@
+
+
+double move_f(int x);//手柄的输入转换曲线（也可以是直线）
+void up(int x);//向前单位
+void down(int x);//向后单位
+void left(int x);//左转
+void right(int x);//右转
+void gofront(int x);//炮台向前
+void gotial(int x);//炮台向后
+void shoot();
+void motor1(int x);//单独控制电机模块
+void motor2(int x);
+void motor3(int x);
+void motor4(int x);
+
+void setup() {
+  // put your setup code here, to run once:
+   
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
